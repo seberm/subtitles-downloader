7ee266e17418a51002ec530efafe9f9db5820306
Sabart Otto <seberm@gmail.com>
2013-08-29 23:46:15 +0200

    Getting information about subtitles

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ subtitles-downloader
 ====================
 
 Subtitles downloader (downloads titles from OpenSubtitles.com)
+
+Dependencies
+============
+python-magic
+python-opensubtitles
--- subs.py
+++ subs.py
@@ -26,23 +26,88 @@ class Data(object):
 
 
 VERSION = '0.1-beta'
+DEFAULT_LOGGING_LEVEL = 'info'
+DEFAULT_LOGGING_FORMAT = '%(levelname)s: %(message)s'
+
+DEFAULT_SUBTITLES_LANGUAGE = 'eng'
+VIDEO_TYPES = ['mp4', 'avi', 'wmv']
+
+
+def videoFiletype(file):
+    postfix = file.split(".")[-1]
+    if postfix in VIDEO_TYPES:
+        return True
+    else:
+        return False
+
+
 
 class Manager:
-    def __init__(self):
-        self.fd = OpenSubtitles()
+    __fd = None
+
+
+    def __init__(self, args, language=DEFAULT_SUBTITLES_LANGUAGE):
+        debug('Logging to OpenSubtitles.org API server')
+        self.__fd = OpenSubtitles()
+        self.__recursiveDownload = False
+        self.__language = language
+        debug('Subtitle language is set to [%s]' % self.__language)
+
+        self.__args = args
 
 
     def __del__(self):
-        self.fd.logout()
+        debug('Logouting from OpenSubtitles.org API server')
+        self.__fd.logout()
+
+
+    def login(self, login, password):
+        token = self.__fd.login(login, password)
+        if not token:
+            raise Exception('Can\'t login to OpenSubtitles.org')
+
+        debug('Login token %s' % token)
+
+
+    def setRecursiveDownload(self, opt=True):
+        self.__recursiveDownload = opt
+
+
+    def download(self):
+        for arg in self.__args:
+            self.__downloadSubtitles(arg)
+
+
+    def __downloadSubtitles(self, path):
+        '''Download subtitles for every movie in specified directory'''
+
+        if os.path.isdir(path):
+            for f in os.listdir(path):
+                if self.__recursiveDownload:
+                    self.__downloadSubtitles(os.path.join(path, f))
+
+                if not videoFiletype(f):
+                    continue
+                else:
+                    debug('Finding titles for: %s' % f)
+
+                    from pythonopensubtitles.utils import File
+                    movie = File(os.path.join(path, f))
+                    debug("[%s] Hash: %s" % (f, movie.get_hash()))
+                    debug("[%s] Size: %d Bytes" % (f, movie.size))
+                    subtitleData = self.__fd.search_subtitles([{'sublanguageid': self.__language, 'moviehash': movie.get_hash(), 'moviebytesize': movie.size}])
+                    debug("[%s] Found %d subtitles" % (f, len(subtitleData)))
+
+                    # Get subtitles with the big number of downloads
+                    bestSubtitles = None
+                    for subs in subtitleData:
+                        if not bestSubtitles or int(subs['SubDownloadsCnt']) > int(bestSubtitles['SubDownloadsCnt']):
+                            bestSubtitles = subs
+
+                    print(bestSubtitles['SubDownloadsCnt'])
 
         '''
-        from pythonopensubtitles.utils import File
-        f = File(os.path.join(Data.path, Data.video))
-        h = f.get_hash()
-        print("Hash: %s" % h)
-        print("Size: %f" % f.size)
 
-        data = fd.search_subtitles([{'sublanguageid': 'cze', 'moviehash': h, 'moviebytesize': f.size}])
 
         import urllib2
 
@@ -63,15 +128,6 @@ class Manager:
 
 
 
-    def login(self, login, password):
-        token = self.fd.login(login, password)
-        if not token:
-            raise Exception('Can\'t login to OpenSubtitles.org')
-
-        debug('Login token %s' % token)
-
-
-
 
 
 def main():
@@ -81,16 +137,44 @@ def main():
                           version = '%prog' + VERSION)
 
     options = OptionGroup(parser, 'Options')
-    options.add_option('-r', '--recursive', dest='recursive', action='store_true',
+    options.add_option('-r', '--recursive', dest='recursiveDownload', action='store_true', default=False,
                        help='Recursive download throught directories')
+    options.add_option('-l', '--language', dest='language', action='store', default=DEFAULT_SUBTITLES_LANGUAGE,
+                       help='Subtitles language (default: eng)')
+    options.add_option('-d', '--dest-dir', dest='destinationDir', action='store',
+                       help='Directory where subtitles are saved')
+    options.add_option('--log', dest='logLevel', action='store', default=DEFAULT_LOGGING_LEVEL,
+                        help='Set logging level (debug, info, warning, error, critical)')
 
 
     parser.add_option_group(options)
     (opt, args) = parser.parse_args()
 
 
-    manager = Manager()
+    # Logging stuff
+    try:
+        logging.basicConfig(format=DEFAULT_LOGGING_FORMAT, level=opt.logLevel.upper())
+        debug('Setting logging mode to: %s' % opt.logLevel.upper())
+    except ValueError:
+        logging.basicConfig(format=DEFAULT_LOGGING_FORMAT, level=DEFAULT_LOGGING_LEVEL)
+        warning('It is not possible to set logging level to %s' % opt.logLevel.upper())
+        warning('Using default setting logging level: %s' % DEFAULT_LOGGING_LEVEL)
+
+
+
+    if opt.destinationDir:
+        debug('Changing default program directory to %s' % opt.destinationDir)
+        os.chdir(opt.destinationDir)
+
+    if not args:
+        error('It\'s necessary to provide at least one argument')
+        sys.exit(1)
+
+    manager = Manager(args, language=opt.language)
     manager.login(Data.username, Data.password)
+    manager.setRecursiveDownload(opt.recursiveDownload)
+
+    manager.download()
 
 
 
