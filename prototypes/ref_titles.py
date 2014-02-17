af34ca470b0f694e08704704cb0903f846128bd3
Otto Sabart <ottosabart@seberm.com>
2014-02-17 20:48:10 +0100

    Prototype of sampling

--- prototypes/ref_titles.py
+++ prototypes/ref_titles.py
@@ -5,29 +5,38 @@ import pysrt
 import math
 
 subsE = pysrt.open('./eng.srt', encoding='utf-8')
-#subsC = pysrt.open('./cze-utf8.srt', encoding='utf-8')
-subsC = pysrt.open('./nesedi-utf8.srt', encoding='utf-8')
+subsC = pysrt.open('./cze-utf8.srt', encoding='utf-8')
+#subsC = pysrt.open('./nesedi-utf8.srt', encoding='utf-8')
+#subsC = pysrt.open('./kratke.srt', encoding='utf-8')
 
-#cmp1 = []
-#offset = 60  # sekund
-#time = 0
-#
+vzorkE = []
+time = 0
+vzk = 30 # sekund (vzorek)
+
 # Zkouska vzorkovani
-#for titles in subs:
-#    if titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds > time+offset:
-#        cmp1.append(titles)
-#        #print("%d:%d:%d\t: %s" % (titles.start.hours, titles.start.minutes, titles.start.seconds, titles.text))
-#        time = titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds + offset
+for titles in subsE:
+    if titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds > time:
+        vzorkE.append(titles)
+        time = titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds + vzk
+
+vzorkC = []
+time = 0
+
+# Zkouska vzorkovani
+for titles in subsC:
+    if titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds > time:
+        vzorkC.append(titles)
+        time = titles.start.hours * 60 * 60 + titles.start.minutes * 60 + titles.start.seconds + vzk
 
 
 score = 0
-offset = 1 # vteriny
+offset = 1.0 # vteriny
 
-for eSub in subsE:
+for eSub in vzorkE:
     startSecsEng = eSub.start.hours * 60 * 60 + eSub.start.minutes * 60 + eSub.start.seconds
     endSecsEng = eSub.end.hours * 60 * 60 + eSub.end.minutes * 60 + eSub.end.seconds
 
-    for cSub in subsC:
+    for cSub in vzorkC:
         startSecsCze = cSub.start.hours * 60 * 60 + cSub.start.minutes * 60 + cSub.start.seconds
         endSecsCze = cSub.end.hours * 60 * 60 + cSub.end.minutes * 60 + cSub.end.seconds
         
